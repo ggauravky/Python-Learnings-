6e9bc27b8d3ad0f00c579116bf47dcd6a2b498ce
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2025-10-29 23:53:45 +0530

    learned objects - mutable and immutable

---
+++ 02_Datatypes/001 chapter.py
@@ -0,0 +1,11 @@
+sugar_amount=2
+print(f"initial sugar:{sugar_amount}")
+print(f"initial sugar:{id(2)}")
+print(f"initial sugar:{id(sugar_amount)}")
+
+
+sugar_amount=24
+print(f"second initial sugar:{sugar_amount}")
+print(f"second initial sugar:{id(24)}")
+print(f"second initial sugar:{id(sugar_amount)}")
+
