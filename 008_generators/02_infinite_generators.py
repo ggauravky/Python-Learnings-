02f69c12b84233f9136ff67b53400ef101220fa5
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-07-14 23:15:03 +0530

    Update 02_infinite_generators.py

--- 008_generators/02_infinite_generators.py
+++ 008_generators/02_infinite_generators.py
@@ -12,3 +12,16 @@ for _ in range(5):
 
 for _ in range(6):
     print(next(user2))
+
+
+#more examples of infinite generators
+set={1, 2, 3, 4, 5}
+def infinite_set():
+    while True:
+        for i in set:
+            yield i
+
+infinite_set_gen = infinite_set()
+for _ in range(10):
+    print(next(infinite_set_gen))   
+    
